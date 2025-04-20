0e88daf14ac3014d03a4bdce75b4b2109348c320
Alex Schenk <134595144+alx-sch@users.noreply.github.com>
2025-04-20 21:51:03 +0000

    spacing

--- CPP_01/ex00/main.cpp
+++ CPP_01/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/07 15:37:54 by aschenk           #+#    #+#             */
-/*   Updated: 2025/04/20 21:04:14 by codespace        ###   ########.fr       */
+/*   Updated: 2025/04/20 21:36:49 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,12 @@
 Zombie* newZombie(std::string name);
 void randomChump(std::string name);
 
-int main() {
+int main( void ) {
     Zombie* z = newZombie("Heap");
+    
     z->announce();  
-
     randomChump("Stack"); 
-
-    delete z; 
+    delete z;
+     
     return 0;
 }   
