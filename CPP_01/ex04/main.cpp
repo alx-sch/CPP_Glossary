711ef8198390b9e0c870ed55c6df222850595a0b
Alexander Schenk <alx-sch@users.noreply.github.com>
2025-04-25 12:42:57 +0200

    use getline() instead of oss for file readin

--- CPP_01/ex04/main.cpp
+++ CPP_01/ex04/main.cpp
@@ -6,14 +6,13 @@
 /*   By: aschenk <aschenk@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/23 11:12:42 by aschenk           #+#    #+#             */
-/*   Updated: 2025/04/23 17:11:13 by aschenk          ###   ########.fr       */
+/*   Updated: 2025/04/25 12:40:41 by aschenk          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <cstring>    // For strerror
 #include <fstream>    // For file I/O
 #include <iostream>
-#include <sstream>    // For stringstream
 
 std::string	replaceAll(std::string content, std::string s1, std::string s2);
 
@@ -24,19 +23,6 @@ int	main(int argc, char **argv) {
 		return 1;
 	}
 
-	// Open filestream
-	std::ifstream input(argv[1]);
-	if (!input) {
-		std::cerr << "Error opening file '" << argv[1] << "': " << strerror(errno) << std::endl;
-		return 1;
-	}
-
-	// Load file content into a string
-	std::ostringstream	oss;
-	oss << input.rdbuf(); // reads entire content of 'input' and stores it in 'oss'
-	std::string	content = oss.str(); // extracts full string from stream buffer
-	input.close();
-
 	std::string	s1 = argv[2];
 	std::string	s2 = argv[3];
 
@@ -46,10 +32,27 @@ int	main(int argc, char **argv) {
 		return 1;
 	}
 
+	// Open input file
+	std::ifstream input(argv[1]);
+	if (!input) {
+		std::cerr << "Error opening file '" << argv[1] << "': " << strerror(errno) << std::endl;
+		return 1;
+	}
+
+	// Load file content into a string
+	std::string	content;
+	std::string	line;
+
+	// Read each line and append it to 'content'
+	while (std::getline(input, line)) {
+    	content += line + "\n";  // Add each line followed by a newline character
+	}
+	input.close();	
+
 	std::string replaced = replaceAll(content, s1, s2);
 
 	// Load 'replaced' string into output file.
-	std::ofstream output((std::string(argv[1]) + ".replace").c_str()); // In C++98, std::ofstream only takes a const char* as the filename argument
+	std::ofstream	output((std::string(argv[1]) + ".replace").c_str()); // In C++98, std::ofstream only takes a const char* as the filename argument
 	if (!output) {
 		std::cerr << "Error: cannot open output file.\n";
 		return 1;
