808e2416cf9271ef8f3f63142fb3b7d9d7f935c5
Alex Schenk <134595144+alx-sch@users.noreply.github.com>
2025-04-28 10:52:32 +0000

    Add info on Fixed-Point Numbers

--- CPP_02/ex01/Fixed.cpp
+++ CPP_02/ex01/Fixed.cpp
@@ -6,9 +6,19 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/24 13:33:35 by aschenk           #+#    #+#             */
-/*   Updated: 2025/04/27 21:39:32 by codespace        ###   ########.fr       */
+/*   Updated: 2025/04/28 10:51:54 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/**
+A fixed-point is just an integer here you pretend some bits are after
+a decimal point.
+The last 8 bits are the fractional part, and the rest is the integer part
+(int got 32 bits, so 32 - 8 = 24 bits for the integer part).
+
+8 bits can represent 256 values, so the range of the fractional part is
+(0/256 to 255/256) -> (0/256 to 255/256); where 255/256 equals 0.99609375.
+ */
 
 #include <iostream>
 #include <cmath>		// for roundsf()
@@ -20,10 +30,15 @@ Fixed::Fixed() : fp_val(0) {
 	std::cout << "Default constructor called\n";
 }
 
+// Integer value is shifted left by 8 bits to make space for the fractional part.
+// Shifting left by 8 bits is equivalent to multiplying by 256 (2^8).
 Fixed::Fixed(int const val) : fp_val(val << fract_bits) {
 	std::cout << "Int constructor called\n";
 }
 
+// Bit-wise shifting is only valid for integers.
+// The float value is multiplied by 256 (1<<8) to convert it to fixed-point representation.
+// roundf() is used to round the float value to the nearest integer.
 Fixed::Fixed(float const val) : fp_val(roundf(val * (1 << fract_bits))) {
 	std::cout << "Float constructor called\n";
 }
@@ -54,10 +69,14 @@ std::ostream &operator<<(std::ostream &out, const Fixed &fixed) {
 
 // Conversion functions
 
+// Right-shift the fixed-point value by 8 bits to convert it to an integer.
+// Basically: int = fixed-point value / 256
 int		Fixed::toInt() const {
 	return fp_val >> fract_bits;
 }
 
+// Divide the fixed-point value by 256 (1<<8) to convert it to a float.
+// static_cast<float>(fp_val): C++ type casting.
 float	Fixed::toFloat() const {
 	return static_cast<float>(fp_val) / (1 << fract_bits);
 }
