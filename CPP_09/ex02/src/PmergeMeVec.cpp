c3c32b16596157bcdca5c5e26f0273c471173d1c
Alexander Schenk <alx-sch@users.noreply.github.com>
2025-08-19 17:17:32 +0200

    rename var

--- CPP_09/ex02/src/PmergeMeVec.cpp
+++ CPP_09/ex02/src/PmergeMeVec.cpp
@@ -1,7 +1,10 @@
-#include <cstdlib>	// atoi()
+#include <cstdlib>		// atoi()
+#include <algorithm>	// swap()
 #include <vector>
 
 #include "../include/PmergeMe.hpp"
+
+static void	fordJohnson(std::vector<int> &vec);
 
 std::vector<int>	PmergeMe::sortVec(int argc, char** argv, int& numComp)
 {
@@ -14,7 +17,67 @@ std::vector<int>	PmergeMe::sortVec(int argc, char** argv, int& numComp)
 		vec.push_back(std::atoi(argv[i]));
 	}
 
+	fordJohnson(vec);
+
+
 	++numComp;
 
 	return vec;
+}
+
+static void	fordJohnson(std::vector<int> &vec)
+{
+	fordJohnsonSort(vec, 0, vec.size());
+}
+
+////////////
+// HELPER //
+////////////
+
+// Pair elements in [start,end), smaller first in each pair
+// Returns: number of pairs (pending size), leftover = last element if odd
+static void	pairAndSplit(std::vector<int> &vec, size_t start, size_t end,
+	size_t &numPairs, int &leftover)
+{
+	size_t		n = end - start;
+	leftover = -1;
+
+	// Pair elements and sort within pairs
+	for (size_t i = 0; i + 1 < n; i += 2)
+	{
+		if (vec[i] > vec[i+1])
+			std::swap(vec[i], vec[i+1]); // smaller first, larger second
+	}
+
+	numPairs = n / 2; // number of pairs (pending size)
+	if (n % 2 != 0)
+	{
+		leftover = vec[n-1]; // last element is leftover (unpaired)
+	}
+}
+
+static void	fordJohnsonSort(std::vector<int> &vec, size_t start, size_t end)
+{
+	size_t	n = end - start;
+	if (n <= 1) // Base case
+	{
+		return;
+	}
+
+	size_t	numPairs; // size_t as added to start (which is size_t)
+	int		leftover;
+
+	// [42 17 99 5 8 23 7]
+	pairAndSplit(vec, start, end, numPairs, leftover);
+	// [17,42  5,99  8,23  7]; numPairs = 3, leftover = 7
+
+	// Step 1: recursively sort the main chain (larger element of each pair)
+	size_t	mainEnd = end;
+	if (leftover != -1)
+	{
+		mainEnd = end - 1; // Exclude leftover from main chain
+	}
+
+	fordJohnsonSort(vec, start + numPairs, mainEnd);
+
 }
